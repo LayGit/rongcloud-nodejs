f059dee5aa6c297a8ab1c28c8e796f8c8dfa1e5d
Lay <jserlay@gmail.com>
2015-02-28 16:46:19 +0800

    Bug Fixed

--- src/rongAPI.js
+++ src/rongAPI.js
@@ -1,8 +1,9 @@
 var Request = require('request');
 var Crypto = require("crypto");
 var SERVER_URL = 'https://api.cn.rong.io/';
+
 /**
- * 融云API for nodejs 1.0
+ * 融云API for nodejs 0.0.2
  * @author JserLay
  * @created 2015-02-28
  */
@@ -10,13 +11,46 @@ var RongAPI = function(appKey, appSecret, format){
     this.appKey = appKey;
     this.appSecret = appSecret;
     this.format = format || 'json';
-};
 
-RongAPI.prototype = {
+    /**
+     * 请求融云服务端
+     * @param action
+     * @param params
+     * @param callback
+     */
+    this.request = function(action, params, callback){
+        action = action.charAt(0) == '/' ? action : '/' + action;
+
+        var Nonce = getRandomNum(10000, 99999),
+            Timestamp = (new Date()).getTime().toString().substr(0, 10),
+            sign = sha1(this.appSecret + Nonce + Timestamp);
+
+        var options = {
+            url:SERVER_URL + action,
+            method:'POST',
+            useQuerystring:true,
+            headers:{
+                'App-Key':this.appKey,
+                'Nonce':Nonce,
+                'Timestamp':Timestamp,
+                'Signature':sign
+            }
+        };
+        if (params)
+            options.form = params;
+        Request(options, function(error, response, body){
+            if (error)
+                throw  error;
+            else
+                callback ? callback(body) : 0;
+        });
+    };
+
+    var that = this;
     /**
      * 用户服务
      */
-    user:{
+    this.user = {
         /**
          * 获取 Token
          * @param userId        用户 Id，最大长度 32 字节。是用户在 App 中的唯一标识码，必须保证在同一个 App 内不重复，重复的用户 Id 将被当作是同一用户。
@@ -25,7 +59,7 @@ RongAPI.prototype = {
          * @param callback      获取完成回调
          */
         getToken:function(userId, name, portraitUri, callback){
-            this.request('/user/getToken', {
+            that.request('/user/getToken', {
                 userId:userId,
                 name:name,
                 portraitUri:portraitUri
@@ -39,7 +73,7 @@ RongAPI.prototype = {
          * @param callback      获取完成回调
          */
         refresh:function(userId, name, portraitUri, callback){
-            this.request('/user/refresh', {
+            that.request('/user/refresh', {
                 userId:userId,
                 name:name,
                 portraitUri:portraitUri
@@ -51,15 +85,16 @@ RongAPI.prototype = {
          * @param callback      检查完成回调
          */
         checkOnline:function(userId, callback){
-            this.request('/user/checkOnline', {
+            that.request('/user/checkOnline', {
                 userId:userId
             }, callback);
         }
-    },
+    };
+
     /**
      * 用户封禁服务
      */
-    userBlock:{
+    this.userBlock = {
         /**
          * 封禁用户
          * @param userId    用户 Id。（必传）
@@ -67,7 +102,7 @@ RongAPI.prototype = {
          * @param callback  完成回调
          */
         block:function(userId, minute, callback){
-            this.request('/user/block', {
+            that.request('/user/block', {
                 userId:userId,
                 minute:minute
             }, callback);
@@ -78,7 +113,7 @@ RongAPI.prototype = {
          * @param callback  完成回调
          */
         unblock:function(userId, callback){
-            this.request('/user/unblock', {
+            that.request('/user/unblock', {
                 userId:userId
             }, callback);
         },
@@ -87,13 +122,14 @@ RongAPI.prototype = {
          * @param callback  完成回调
          */
         query:function(callback){
-            this.request('/user/block/query', null, callback);
+            that.request('/user/block/query', null, callback);
         }
-    },
+    };
+
     /**
-     * 用户黑名单
+     * 黑名单服务
      */
-    userBlackList:{
+    this.userBlackList = {
         /**
          * 添加用户到黑名单
          * @param userId        用户 Id。（必传）
@@ -101,7 +137,7 @@ RongAPI.prototype = {
          * @param callback      完成回调
          */
         add:function(userId, blackUserId, callback){
-            this.request('/user/blacklist/add', {
+            that.request('/user/blacklist/add', {
                 userId:userId,
                 blackUserId:blackUserId
             }, callback);
@@ -113,7 +149,7 @@ RongAPI.prototype = {
          * @param callback      完成回调
          */
         remove:function(userId, blackUserId, callback){
-            this.request('/user/blacklist/remove', {
+            that.request('/user/blacklist/remove', {
                 userId:userId,
                 blackUserId:blackUserId
             }, callback);
@@ -124,15 +160,16 @@ RongAPI.prototype = {
          * @param callback  完成回调
          */
         query:function(userId, callback){
-            this.request('/user/blacklist/query', {
+            that.request('/user/blacklist/query', {
                 userId:userId
             }, callback);
         }
-    },
+    };
+
     /**
      * 消息发送服务
      */
-    message:{
+    this.message = {
         /**
          * 发送单聊消息
          * @param fromUserId    发送人用户 Id。（必传）
@@ -144,7 +181,7 @@ RongAPI.prototype = {
          * @param callback      发送完成回调
          */
         sendPrivate:function(fromUserId, toUserId, objectName, content, pushContent, pushData, callback){
-            this.request('/message/private/publish', {
+            that.request('/message/private/publish', {
                 fromUserId:fromUserId,
                 toUserId:toUserId,
                 objectName:objectName,
@@ -164,7 +201,8 @@ RongAPI.prototype = {
          * @param callback      发送完成回调
          */
         sendSystem:function(fromUserId, toUserId, objectName, content, pushContent, pushData, callback){
-            this.request('/message/system/publish', {
+            if (toUserId)
+            that.request('/message/system/publish', {
                 fromUserId:fromUserId,
                 toUserId:toUserId,
                 objectName:objectName,
@@ -184,7 +222,7 @@ RongAPI.prototype = {
          * @param callback      发送完成回调
          */
         sendGroup:function(fromUserId, toGroupId, objectName, content, pushContent, pushData, callback){
-            this.request('/message/group/publish', {
+            that.request('/message/group/publish', {
                 fromUserId:fromUserId,
                 toGroupId:toGroupId,
                 objectName:objectName,
@@ -202,7 +240,7 @@ RongAPI.prototype = {
          * @param callback      发送完成回调
          */
         sendChatroom:function(fromUserId, toChatroomId, objectName, content, callback){
-            this.request('/message/chatroom/publish', {
+            that.request('/message/chatroom/publish', {
                 fromUserId:fromUserId,
                 toChatroomId:toChatroomId,
                 objectName:objectName,
@@ -217,24 +255,25 @@ RongAPI.prototype = {
          * @param callback      完成回调
          */
         sendBroadcast:function(fromUserId, objectName, content, callback){
-            this.request('/message/broadcast', {
+            that.request('/message/broadcast', {
                 fromUserId:fromUserId,
                 objectName:objectName,
                 content:content
             }, callback);
         }
-    },
+    };
+
     /**
      * 消息历史记录服务
      */
-    messageHistory:{
+    this.messageHistory = {
         /**
          * 消息历史记录服务
          * @param date      指定北京时间某天某小时，格式为2014010101,表示：2014年1月1日凌晨1点。（必传）
          * @param callback  获取完成回调
          */
         query:function(date, callback){
-            this.request('/message/history', {
+            that.request('/message/history', {
                 date:date
             }, callback);
         },
@@ -244,15 +283,16 @@ RongAPI.prototype = {
          * @param callback  完成回调
          */
         delete:function(date, callback){
-            this.request('/message/history/delete', {
+            that.request('/message/history/delete', {
                 date:date
             }, callback);
         }
-    },
+    };
+
     /**
      * 群组服务
      */
-    group:{
+    this.group = {
         /**
          * 同步用户所属群组
          * @param userId    被同步群信息的用户Id。（必传）
@@ -266,7 +306,7 @@ RongAPI.prototype = {
             if (groups)
                 for (var g in groups)
                     params[g] = groups[g];
-            this.request('/group/sync', params, callback);
+            that.request('/group/sync', params, callback);
         },
         /**
          * 创建群组
@@ -276,7 +316,7 @@ RongAPI.prototype = {
          * @param callback  完成回调
          */
         create:function(userId, groupId, groupName, callback){
-            this.request('/group/create', {
+            that.request('/group/create', {
                 userId:userId,
                 groupId:groupId,
                 groupName:groupName
@@ -290,7 +330,7 @@ RongAPI.prototype = {
          * @param callback      完成回调
          */
         join:function(userId, groupId, groupName, callback){
-            this.request('/group/join', {
+            that.request('/group/join', {
                 userId:userId,
                 groupId:groupId,
                 groupName:groupName
@@ -303,7 +343,7 @@ RongAPI.prototype = {
          * @param callback  完成回调
          */
         quit:function(userId, groupId, callback){
-            this.request('/group/quit', {
+            that.request('/group/quit', {
                 userId:userId,
                 groupId:groupId
             }, callback);
@@ -315,7 +355,7 @@ RongAPI.prototype = {
          * @param callback  完成回调
          */
         dismiss:function(userId, groupId, callback){
-            this.request('/group/dismiss', {
+            that.request('/group/dismiss', {
                 userId:userId,
                 groupId:groupId
             }, callback);
@@ -327,23 +367,24 @@ RongAPI.prototype = {
          * @param callback      完成回调
          */
         refresh:function(groupId, groupName, callback){
-            this.request('/group/refresh', {
+            that.request('/group/refresh', {
                 groupId:groupId,
                 groupName:groupName
             }, callback);
         }
-    },
+    };
+
     /**
      * 聊天室服务
      */
-    chatroom:{
+    this.chatroom = {
         /**
          * 创建聊天室
          * @param chatrooms 要创建的聊天室 形式：{'chatroom[id]':'name'}
          * @param callback  完成回调
          */
         create:function(chatrooms, callback){
-            this.request('/chatroom/create', chatrooms, callback);
+            that.request('/chatroom/create', chatrooms, callback);
         },
         /**
          * 销毁聊天室
@@ -351,7 +392,7 @@ RongAPI.prototype = {
          * @param callback      完成回调
          */
         destroy:function(chatroomId, callback){
-            this.request('/chatroom/destroy', {
+            that.request('/chatroom/destroy', {
                 chatroomId:chatroomId
             }, callback);
         },
@@ -361,42 +402,10 @@ RongAPI.prototype = {
          * @param callback      完成回调
          */
         query:function(chatroomId, callback){
-            this.request('/chatroom/query', {
+            that.request('/chatroom/query', {
                 chatroomId:chatroomId
             }, callback);
         }
-    },
-    /**
-     * 请求融云服务端
-     * @param action
-     * @param params
-     * @param callback
-     */
-    request:function(action, params, callback){
-        action = action.charAt(0) == '/' ? action : '/' + action;
-
-        var Nonce = getRandomNum(10000, 99999),
-            Timestamp = (new Date()).getTime().toString().substr(0, 10),
-            sign = sha1(this.appSecret + Nonce + Timestamp);
-
-        var options = {
-            url:SERVER_URL + action,
-            method:'POST',
-            headers:{
-                'App-Key':this.appKey,
-                'Nonce':Nonce,
-                'Timestamp':Timestamp,
-                'Signature':sign
-            }
-        };
-        if (params)
-            options.body = JSON.stringify(params);
-        Request(options, function(error, response, body){
-            if (error)
-                throw  error;
-            else
-                callback ? callback(body) : 0;
-        });
     }
 };
 
@@ -421,6 +430,15 @@ function getRandomNum(Min,Max)
 function sha1(data)
 {
     return Crypto.createHash('sha1').update(data).digest('hex');
+}
+
+/**
+ * 判断是否数组
+ * @param obj
+ */
+function isArray(obj)
+{
+
 }
 
 module.exports = RongAPI;
